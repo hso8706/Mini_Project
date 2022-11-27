a38e11fec7e6915bddb4beb2ca75a81a0f2ba45d
jumgho <103169947+jumgho@users.noreply.github.com>
2022-11-27 18:01:24 +0900

    AllClone

---
+++ AllClone/src/test/java/com/example/AllClone/AllCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AllClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
