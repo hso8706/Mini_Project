a0ec2c0ced372077fded8fa8f2e4730b112626eb
jumgho <103169947+jumgho@users.noreply.github.com>
2022-11-29 23:35:53 +0900

    22.11.29

--- diary/src/main/java/com/example/diary/content/entity/Content.java
+++ diary/src/main/java/com/example/diary/content/entity/Content.java
@@ -5,10 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.domain.Auditable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -21,8 +18,10 @@ public class Content{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long contentId; // repository 에서 사용하기 위해 래퍼클래스 사용, 근데 primary 를 쓰면 어차피 여러모로 불편함.
     //TODO writer
+    @Column(nullable = false, length = 30)
     private String writer;
     //TODO text
+    @Column(nullable = false)
     private String text;
     //TODO createdAt
     private LocalDateTime createdAt = LocalDateTime.now();
